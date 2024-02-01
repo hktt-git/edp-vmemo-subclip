5b9c450688556deb28fdb976c6b80981d20cc4a4
k.hayashi <kazuki618@384.jp>
2024-02-01 22:49:44 +0900

    modify: リファクタリング＆終了＆ファイル操作改善

--- player/main.py
+++ player/main.py
@@ -1,191 +1,239 @@
-from moviepy.editor import *
-from moviepy.video.fx.resize import resize
 import threading
 import time
+import glob
+
 import pygame
 from pygame.locals import *
 import numpy as np
-import serial
+
+from moviepy.editor import *
+
 import serial.tools.list_ports
+import serial
 
-MOVIE_DIRECTORY = "./movie"
-OUTPUT_DIRECTORY = "./output"
-OUTPUT_CLIP_DURATION = 5
+from dotenv import load_dotenv
+import os
 
-def imdisplay(imarray, screen=None):
-    """Splashes the given image array on the given pygame screen """
-    a = pygame.surfarray.make_surface(imarray.swapaxes(0, 1))
-    if screen is None:
-        screen = pygame.display.set_mode(imarray.shape[:2][::-1])
-    # screen.blit(a, (0, 0))
-    pygame.transform.scale(a, DISPLAY_SIZE, screen)
+import shutil
 
+import queue
 
-ports = list(serial.tools.list_ports.comports())
-for p in ports:
-    print(p)
-    print(" device       :", p.device)
-    print(" name         :", p.name)
-    print(" description  :", p.description)
-    print(" hwid         :", p.hwid)
-    print(" vid          :", p.vid)
-    print(" pid          :", p.pid)
-    print(" serial_number:", p.serial_number)
-    print(" location     :", p.location)
-    print(" manufactuer  :", p.manufacturer)
-    print(" product      :", p.product)
-    print(" interface    :", p.interface)
-    print("")
+TEMP_DIR = "./temp"
+TEMP_OUTPUT_DIR = f"{TEMP_DIR}/output"
+temp_playng_dir = f"{TEMP_DIR}/playing"
+temp_editing_dir = f"{TEMP_DIR}/editing"
 
-if not len(ports):
-   print("Serial device not found")
-   exit()
+def open_playing_clip(movie_directory):
+    movie_files = glob.glob(f"{movie_directory}/*.mp4")
 
-portname = ports[0].name
+    clips = list(map(lambda file: VideoFileClip(file), movie_files))
 
-serialPort = serial.Serial(portname, 115200, timeout=0)
+    names = list(map(os.path.basename, movie_files))
 
-# while True: 
-#   line = serialPort.readline()
+    timestamps = list()
 
-#   if not len(line):
-#     time.sleep(0.1)
-#     continue
+    t = 0
 
-#   print(line)
+    for clip in clips:
+        t += clip.duration
+        timestamps.append(t)
 
-pygame.init()
+    merged_clip = concatenate_videoclips(clips)
 
-DISPLAY_SIZE = pygame.display.get_desktop_sizes()[-1]
-print(DISPLAY_SIZE)
+    aud = merged_clip.audio.set_fps(clips[0].audio.fps)
+    merged_clip = merged_clip.without_audio().set_audio(aud)
 
-screen = pygame.display.set_mode(DISPLAY_SIZE, FULLSCREEN & SCALED)
+    return (merged_clip, list(map(lambda x,y: (x,y), names, timestamps)))
 
-circle_position = pygame.Vector2()
+time_ref = 0
 
-clock = pygame.time.Clock()
-
-F_RATE = 30
-
-font = pygame.font.SysFont("Yu Gothic", 50, True)
-
-text = font.render("Vmemoを記録中です", True, (0,0,200))
-
-TEXT_VISIBLE_DURATION = 2 * 1e3
-
-text_start = None
-
-
-clip = VideoFileClip(f"{MOVIE_DIRECTORY}/sample_copy.mp4")
-video_for_edit = VideoFileClip(f"{MOVIE_DIRECTORY}/sample.mp4")
-
-# clip = resize(clip, DISPLAY_SIZE)
-fps=15
-
-audio = clip.audio
-audio_fps=22050
-audio_buffersize=3000
-audio_nbytes=2
-
-# the sound will be played in parrallel. We are not
-# parralellizing it on different CPUs because it seems that
-# pygame and openCV already use several cpus it seems.
-
-# two synchro-flags to tell whether audio and video are ready
-videoFlag = threading.Event()
-audioFlag = threading.Event()
-# launch the thread
-audiothread = threading.Thread(target=clip.audio.preview,
-                                args=(audio_fps,
-                                      audio_buffersize,
-                                      audio_nbytes,
-                                      audioFlag, videoFlag))
-audiothread.start()
-
-img = clip.get_frame(0)
-imdisplay(img, screen)
-if audio:  # synchronize with audio
-    videoFlag.set()  # say to the audio: video is ready
-    audioFlag.wait()  # wait for the audio to be ready
-
-t0 = time.time()
-
-counter = 1
-
-for t in np.arange(1.0 / fps, clip.duration-.001, 1.0 / fps):
+def player(clip: VideoClip, screen: pygame.Surface, lock: threading.RLock, flag: threading.Event, fps=15):
+    global time_ref
     
-    img = clip.get_frame(t)
+    def imdisplay(imarray, screen):
+        a = pygame.surfarray.make_surface(imarray.swapaxes(0, 1))
+        b = screen.copy()
+        pygame.transform.scale(a, b.get_size(), b)
+        with lock:
+            screen.blit(b, (0, 0))
+
+    videoFlag = threading.Event()
+    audioFlag = threading.Event()
+
+    audio = clip.audio
+    audio_fps=22050
+    audio_buffersize=3000
+    audio_nbytes=2
+
+    audiothread = threading.Thread(target=clip.audio.preview,
+                                    args=(audio_fps,
+                                          audio_buffersize,
+                                          audio_nbytes,
+                                          audioFlag, videoFlag))
+    audiothread.start()
+
+    img = clip.get_frame(0)
+
+    imdisplay(img, screen)
     
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT or \
-                (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+    if audio:  # synchronize with audio
+        videoFlag.set()  # say to the audio: video is ready
+        audioFlag.wait()  # wait for the audio to be ready
+
+    t0 = time.time()
+
+    for t in np.arange(1.0 / fps, clip.duration-.001, 1.0 / fps):
+        img = clip.get_frame(t)
+
+        t1 = time.time()
+        time.sleep(max(0, t - (t1-t0)))
+
+        imdisplay(img, screen)
+        time_ref = t
+
+        if flag.is_set():
             if audio:
                 videoFlag.clear()
-            print("Interrupt")
-            exit()
-                
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            x, y = pygame.mouse.get_pos()
-            rgb = img[y, x]
-            print("time, position, color : ", "%.03f, %s, %s" %
-                  (t, str((x, y)), str(rgb)))
-                
-    t1 = time.time()
-    time.sleep(max(0, t - (t1-t0)))
-    imdisplay(img, screen)
+            return
 
-    if text_start is not None and pygame.time.get_ticks() - text_start < TEXT_VISIBLE_DURATION:
-      rect_size = (text.get_width(), text.get_height())
-      rect_upper_left_pos = (0, 0)
-      pygame.draw.rect(screen, (255,255,255), (DISPLAY_SIZE[0] - rect_size[0] - rect_upper_left_pos[0], rect_upper_left_pos[1], rect_size[0], rect_size[1]))
-      screen.blit(text, (DISPLAY_SIZE[0] - rect_size[0] - rect_upper_left_pos[0], rect_upper_left_pos[1]))
-    else:
-      text_start = None
+def open_serial():
+    ports = list(serial.tools.list_ports.comports())
 
-    serialData = serialPort.readline()
+    if not len(ports):
+        print("Serial device not found")
+        exit()
 
-    if len(serialData):
-       code = int(serialData)
-       if code == 73:
-        text_start = pygame.time.get_ticks()
+    portname = ports[0].name
+    print(portname)
 
-        def task():
-          global counter
-          clip_end_position = t1 - t0
-          print(clip_end_position)
-          clip_start_position = max(clip_end_position - OUTPUT_CLIP_DURATION, 0)
-          clipped_video = video_for_edit.subclip(clip_start_position, clip_end_position)
-          clipped_video.write_videofile(f"{OUTPUT_DIRECTORY}/output{counter}.mp4")
-          counter += 1
+    serialPort = serial.Serial(portname, 115200, timeout=0)
 
-        thread = threading.Thread(target=task)
-        thread.start()
+    return serialPort
 
-    pygame.display.flip()
+def subclipper(request: queue.Queue, clip: VideoClip, clip_names: list, duration: float, output: str):
+    while True:
+        clip_end_position = request.get()
 
+        print(clip_end_position)
 
-while True:
-  clock.tick(F_RATE)
-  
-  screen.fill((0, 0, 0))
-  
-  pygame.draw.circle(screen, (255, 255, 255), circle_position, 20)
-  
-  if text_start is not None and pygame.time.get_ticks() - text_start < TEXT_VISIBLE_DURATION:
-    screen.blit(text, (40,30))
-  else:
-    text_start = None
-  
-  pygame.display.update()
-  
-  circle_position += 1, 1
+        clip_start_position = max(clip_end_position - duration, 0)
+        
+        clipped_video = clip.subclip(clip_start_position, clip_end_position)
+        
+        output_filename = "output.mp4"
+        
+        for name, timestamp in clip_names:
+            output_filename = name
+            
+            if clip_start_position < timestamp:
+                break
 
-  for event in pygame.event.get():
-    if event.type == QUIT:
-      pygame.quit()
-      exit()
-    if event.type == KEYDOWN:
-      if event.key == K_RETURN:
-        # pygame.draw.rect(screen, (255, 255, 255), (0, 0, 100, 100))
-        text_start = pygame.time.get_ticks()
-        print("enter")
+        temp_file_path = f"{TEMP_OUTPUT_DIR}/{output_filename}"
+        file_path = f"{output}/{output_filename}"
+
+        if not os.path.exists(TEMP_OUTPUT_DIR):
+            os.mkdir(TEMP_OUTPUT_DIR)
+
+        if not os.path.exists(output):
+            os.mkdir(output)
+
+        clipped_video.write_videofile(temp_file_path)
+
+        shutil.copy(temp_file_path, file_path)
+
+        request.get()
+
+def game_loop(clip: VideoClip, serial: serial.Serial, display:int, subclip_req: queue.Queue):
+    global time_ref
+    
+    pygame.init()
+
+    dp_size = pygame.display.get_desktop_sizes()[display]
+    screen = pygame.display.set_mode(dp_size, FULLSCREEN & SCALED, display)
+    
+    player_lock = threading.RLock()
+    player_screen = screen.copy()
+    player_flag = threading.Event()
+    player_thread = threading.Thread(target=player, args=(clip, player_screen, player_lock, player_flag))
+    player_thread.start()
+
+    font = pygame.font.SysFont("Yu Gothic", 50, True)
+    text = font.render("Vmemoを記録中です", True, (0,0,200))
+
+    ticker = pygame.time.Clock()
+
+    def before_return():
+        player_flag.set()
+        pygame.quit()
+
+    while True:
+        ticker.tick(60)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or \
+                    (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
+                print("Interrupt")
+                before_return()
+                return
+
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                print("MOUSEBUTTONDOWN")
+
+        serialData = serial.readline()
+
+        if len(serialData):
+            try:
+                code = int(serialData)
+            except:
+                print("decode error")
+            if code == 73:
+                # text_start = pygame.time.get_ticks()
+                if not subclip_req.qsize():
+                        subclip_req.put(time_ref)
+                        subclip_req.put("owatta?")
+                        print("enqueue")
+
+        with player_lock:
+            screen.blit(player_screen, (0, 0))
+
+        if subclip_req.qsize():
+            rect_size = (text.get_width(), text.get_height())
+            rect_upper_left_pos = (0, 40)
+            pygame.draw.rect(screen, (255,255,255), (dp_size[0] - rect_size[0] - rect_upper_left_pos[0], rect_upper_left_pos[1], rect_size[0], rect_size[1]))
+            screen.blit(text, (dp_size[0] - rect_size[0] - rect_upper_left_pos[0], rect_upper_left_pos[1]))
+
+        pygame.display.flip()
+
+def main():
+    load_dotenv()
+
+    VMEMO_MOVIE_DIR = os.getenv("VMEMO_MOVIE_DIR")
+    VMEMO_OUTPUT_DIR = os.getenv("VMEMO_OUTPUT_DIR")
+    VMEMO_CLIP_DURATION = float(os.getenv("VMEMO_CLIP_DURATION"))
+    VMEMO_DISPLAY = int(os.getenv("VMEMO_DISPLAY"))
+
+    if os.path.exists(TEMP_DIR):
+        shutil.rmtree(TEMP_DIR)
+
+    shutil.copytree(VMEMO_MOVIE_DIR, temp_playng_dir)
+    shutil.copytree(VMEMO_MOVIE_DIR, temp_editing_dir)
+
+    playing_clip, _ = open_playing_clip(temp_playng_dir)
+    editing_clip, clip_names = open_playing_clip(temp_editing_dir)
+
+    print(clip_names)
+
+    serialPort = open_serial()
+
+    subclip_req = queue.Queue()
+    subclipper_thread = threading.Thread(target=subclipper, args=[subclip_req, editing_clip, clip_names, VMEMO_CLIP_DURATION, VMEMO_OUTPUT_DIR])
+    subclipper_thread.start()
+
+    game_loop(playing_clip, serialPort, VMEMO_DISPLAY, subclip_req)
+
+# Ctrl+C 対策
+import signal
+signal.signal(signal.SIGINT, signal.SIG_DFL)
+
+if __name__ == "__main__":
+    main()
